0ca659faa8b1bcceb4b141fb0195d9d5d2aae85f
bradenalexander <bradenalexander@10.236.44.82>
2019-10-09 10:32:16 -0500

    PC-2019.2.3 <bradenalexander@Bradens-MacBook-Pro.local Create laf.xml, filetypes.xml, Python Script.py, editor.codeinsight.xml, Default.xml, project.default.xml, colors.scheme.xml, jdk.table.xml, Default.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Python Script.py
@@ -0,0 +1,22 @@
+# For individual submissions:
+# By submitting this assignment, I agree to the following:
+# “Aggies do not lie, cheat, or steal, or tolerate those who do”
+# “I have not given or received any unauthorized aid on this assignment”
+#
+# Name: YOUR NAME
+# Course/Section: ENGR 102-216
+# Assignment: THE ASSIGNMENT NUMBER (e.g. Lab 1b-2)
+# Date: DAY MONTH YEAR
+
+# For team submissions:
+# By submitting this assignment, all team members agree to the following:
+# “Aggies do not lie, cheat, or steal, or tolerate those who do”
+# “I have not given or received any unauthorized aid on this assignment”
+#
+# Names: NAME OF TEAM MEMBER 1
+# NAME OF TEAM MEMBER 2
+# NAME OF TEAM MEMBER 3
+# NAME OF TEAM MEMBER 4
+# Course/Section: ENGR 102-216
+# Assignment: THE ASSIGNMENT NUMBER
+# Date: DAY MONTH YEAR
